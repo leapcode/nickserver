f5bf2c6554f0e541c75d90fdcf4f0095e9801d4d
Azul <azul@riseup.net>
2016-09-13 16:45:28 +0200

    test: properly test nicknym source + fixes

--- test/remote/nicknym_source_test.rb
+++ test/remote/nicknym_source_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'nickserver/nicknym/source'
+require 'nickserver/email_address'
 
 class RemoteNicknymSourceTest < Minitest::Test
 
@@ -13,8 +14,23 @@ class RemoteNicknymSourceTest < Minitest::Test
     super
   end
 
-  def test_truth
+  def test_availablility_check
     assert source.available_for? 'mail.bitmask.net'
+    refute source.available_for? 'dl.bitmask.net'   # not a provider
+    refute source.available_for? 'demo.bitmask.net' # provider without mx
+  end
+
+  def test_successful_query
+    response = source.query(email_with_key)
+    json = JSON.parse response.content
+    assert_equal 200, response.status
+    assert_equal email_with_key.to_s, json["address"]
+    refute_empty json["openpgp"]
+  end
+
+  def test_not_found
+    response = source.query(email_without_key)
+    assert_equal 404, response.status
   end
 
   protected
@@ -23,5 +39,12 @@ class RemoteNicknymSourceTest < Minitest::Test
     @source ||= Nickserver::Nicknym::Source.new
   end
 
+  def email_with_key
+    Nickserver::EmailAddress.new('test@mail.bitmask.net')
+  end
+
+  def email_without_key
+    Nickserver::EmailAddress.new('pleaseneverusethisemailweuseittotest@mail.bitmask.net')
+  end
 
 end
